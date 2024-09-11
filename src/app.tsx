bd1279c5e0028a2a7a7a69af4512f543fdbfa886
Wesley Medeiros <wesley.medeiros6938@gmail.com>
2024-09-11 17:25:56 -0300

    feat: add empty goals page

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,22 @@
+import logo from './assets/logo-in-orbit.svg'
+import letsStart from './assets/lets-start-illustration.svg'
+import { Plus } from 'lucide-react'
+
 export function App() {
-  return <h1 className="bg-black text-white text-xl">Hello, World!</h1>
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in-orbit" />
+      <img src={letsStart} alt="lets-starts" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        você ainda não tem nenhum meta, que tal cadastrar uma agora mesmo?
+      </p>
+      <button
+        type="submit"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
+      >
+        <Plus className="size-4" />
+        Cadastrar uma meta
+      </button>
+    </div>
+  )
 }
